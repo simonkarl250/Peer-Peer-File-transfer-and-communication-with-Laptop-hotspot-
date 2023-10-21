3fb742a635c680104bdd8755bd7cf2483c140ce3
simonkar250 <simonkar250@yahoo.com>
2023-10-21 14:52:57 +0300

    First Commit

---
+++ src/main/java/com/example/transpro/transpro/TransproApplication.java
@@ -0,0 +1,13 @@
+package com.example.transpro.transpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transpro/transpro/TransproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transpro.transpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
